3e7ea8d206f052a82047b0fea8f89f8b4051d24a
tayson87 <juanpablomartinez37@gmail.com>
2022-07-10 14:41:10 -0500

    project in progress

---
+++ src/utils/Tower.js
@@ -0,0 +1,31 @@
+import Stack from "./Stack";
+
+class Tower {
+  constructor() {
+
+  }
+
+  add() {
+    
+  }
+
+  moveTopTo() {
+    
+  }
+
+  moveDisks() {
+
+  }
+}
+
+let  tower1 = new Tower ();
+let  tower2 = new Tower ();
+let  tower3 = new Tower ();
+
+tower1.add (3);
+ tower1.add(2);
+ tower1.add(1);
+
+ console.log(JSON.stringify(tower1.moveDisks.top));
+
+export default Tower;
